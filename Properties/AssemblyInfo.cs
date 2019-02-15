c02171a5e8ac8924cd4206166da88d54d6d6efc3
Mads Houmann <mads.houmann@outlook.com>
2019-02-15 14:46:12 +0100

    Support for VS 2019 preview

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CommentsPlus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Visual Studio extension - Comments in italics and more.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CommentsPlus")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.8.0")]
+[assembly: AssemblyVersion("1.8.1.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
