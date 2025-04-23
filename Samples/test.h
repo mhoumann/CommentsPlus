364e1eac4b4668bb09a60df867090954c1336daa
Mads Houmann <mhoumann@outlook.com>
2025-04-23 23:19:43 +0200

    Add/edit sample files

---
+++ Samples/test.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#if !defined(TEST_HEADER_H_)
+#define TEST_HEADER_H_
+
+// normal comment
+//! Boldly go
+//!? WAT
+//? Questionable
+
+void whatnot(const void*); 
+
+//+? Unicorns and ...
+
+//x void not_what_not(const void*);
+//// void what_not(void*);
+
+#endif
